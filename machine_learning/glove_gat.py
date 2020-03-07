bc175af83218bffb04e8affdb4490753fcdfdba3
Jason Shau Ji PNg <jsjpng@uwaterloo.ca>
2020-03-07 16:41:35 -0500

    Added assert_vars_change test

--- machine_learning/glove_gat.py
+++ machine_learning/glove_gat.py
@@ -43,11 +43,11 @@ with open("sample_data.pkl", 'rb') as input:
     model_data = pickle.load(input)
 
 model = GloveGAT()
-optimizer = optim.SGD(model.parameters(), lr=0.001)
+optimizer = optim.SGD(model.parameters(), lr=0.0001)
 criterion = nn.MSELoss()
 sub_train, sub_valid = create_train_and_test(model_data)
 
-def update_model(engine, batch):
+def train_step(model, criterion, optimizer, batch):
     adj_matrix = batch["adj_matrix"]
     inputs = batch["embedding"]
     offsets = batch["offsets"]
@@ -60,6 +60,10 @@ def update_model(engine, batch):
     optimizer.step()
     return loss.item()
 
+def update_model(engine, batch):
+    model.train()
+    return train_step(model, criterion, optimizer, batch)
+
 def predict_on_batch(engine, batch):
     model.eval()
     adj_matrix = batch["adj_matrix"]
@@ -69,6 +73,39 @@ def predict_on_batch(engine, batch):
     with torch.no_grad():
         y_pred = model(inputs, offsets, adj_matrix).argmax(1)
     return y_pred, y
+
+
+# Modified version from torchtest to accomodate multiple input variables
+def assert_vars_change(model, loss_fn, optim, batch, device="cpu"):
+    params = [ np for np in model.named_parameters() if np[1].requires_grad ]
+    # take a copy
+    initial_params = [ (name, p.clone()) for (name, p) in params ]
+
+    # run a training step
+    train_step(model, loss_fn, optim, batch)
+
+    # check if variables have changed
+    for (_, p0), (name, p1) in zip(initial_params, params):
+        try:
+            assert not torch.equal(p0.to(device), p1.to(device))
+        except AssertionError:
+            raise Exception(f"{name} did not change!")
+
+def test_variables_change(model, batch):
+    try:
+        assert_vars_change(
+            model=model,
+            loss_fn=criterion,
+            optim=optimizer,
+            batch=batch,
+            device="cpu"
+        )
+        print("SUCCESS: variables changed")
+    except Exception as e:
+        print("FAILED: ", e)
+        exit(1)
+
+test_variables_change(model, model_data[0])
 
 trainer = Engine(update_model)
 evaluator = Engine(predict_on_batch)
@@ -97,5 +134,5 @@ def log_validation_results(trainer):
     print(f"Validation Results - Epoch: {epoch} "\
           f"Avg accuracy: {accuracy:.2f} Avg precision: {precision:.2f} Avg recall: {recall:.2f}")
 
-trainer.run(sub_train, max_epochs=3)
+trainer.run(sub_train, max_epochs=10)
 
