6aed3dd583b136766fdb8758fa1a5d7144ad8def
Jason Shau Ji PNg <jsjpng@uwaterloo.ca>
2020-03-27 18:42:52 -0400

    Added depth to Reddit tree builder, updated tokenizer argument

--- reddit_data/reddit_tree_builder.py
+++ reddit_data/reddit_tree_builder.py
@@ -1,3 +1,4 @@
+import math
 from reddit_data.data_models import RedditComment
 from reddit_data.data_models.reddit_node import RedditNode
 from reddit_data.data_sources.data_source import DataSource
@@ -7,19 +8,19 @@ class RedditTreeBuilder:
     def __init__(self, data_source: DataSource):
         self.data = data_source
 
-    def get_tree_rooted_at(self, parent_comment: RedditComment) -> RedditNode:
-        return self._assign_indexes(self._build_tree(parent_comment))
+    def get_tree_rooted_at(self, parent_comment: RedditComment, depth=math.inf) -> RedditNode:
+        return self._assign_indexes(self._build_tree(parent_comment, depth))
 
-    def _build_tree(self, parent_comment):
-        if parent_comment is None:
+    def _build_tree(self, parent_comment, depth):
+        if parent_comment is None or depth <= 0:
             return None
 
         parent_node = RedditNode(parent_comment)
         child_comments = self.data.get_children(parent_comment.id)
         for child_comment in sorted(child_comments):
-            child_node = self.get_tree_rooted_at(child_comment)
+            child_node = self._build_tree(child_comment, depth - 1)
             if child_node is not None:
-                parent_node.children.append(self.get_tree_rooted_at(child_comment))
+                parent_node.children.append(child_node)
 
         return parent_node
 
