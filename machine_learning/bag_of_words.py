f2a4b7dbb99003855775859823f62085ae42fee5
aparment <a.g.parmentier@gmail.com>
2020-04-14 12:09:00 -0400

    Fixed: Inconsistent tokenizer usage.

--- machine_learning/bag_of_words.py
+++ machine_learning/bag_of_words.py
@@ -6,13 +6,13 @@ from torchtext.data.utils import get_tokenizer
 
 from machine_learning.experiment_types import *
 from machine_learning.feature_extraction import get_tree_text
-from text_preprocessing import stemming_tokenizer
 
-# Methods to extract words
-nltk.download("stopwords")
 
-_eng_stop_words = set(stemming_tokenizer(" ".join(stopwords.words("english")).replace("'", "")))
-tokenizer = get_tokenizer("spacy")
+def setup_bag_of_words():
+    nltk.download("stopwords")
+
+_tokenizer = get_tokenizer("spacy")
+_eng_stop_words = set(_tokenizer(" ".join(stopwords.words("english")).replace("'", "")))
 
 
 def get_bag_of_words(trees, vectorizer_type: VectorizerType):
@@ -26,7 +26,7 @@ def get_bag_of_words(trees, vectorizer_type: VectorizerType):
     else:
         raise Exception("Invalid vectorizer type")
 
-    vectorizer = Vectorizer(stop_words=_eng_stop_words, tokenizer=stemming_tokenizer, min_df=0.001)
+    vectorizer = Vectorizer(stop_words=_eng_stop_words, tokenizer=_tokenizer, min_df=0.001)
     word_freq = vectorizer.fit_transform(flat_texts)
 
     num_nodes = [0] + [len(text) for text in texts]
